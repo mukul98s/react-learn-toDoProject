eea711c41a1c0b3195938b6c2f50c2d0a0f3813b
Mukul Sharma <mymukul112@gmail.com>
2021-04-21 19:10:00 +0530

    completed: add/delete Functionality

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,12 +1,35 @@
 import React from "react";
-const TaskItem = ({ task }) => {
+const TaskItem = ({ task, item, todo, setTodo }) => {
+  const deleteHandler = () => {
+    setTodo(
+      todo.filter((element) => {
+        return element.id !== item.id;
+      })
+    );
+  };
+
+  const completeHandler = () => {
+    setTodo(
+      todo.map((element) => {
+        if (element.id === item.id)
+          return {
+            ...item,
+            completed: !item.completed,
+          };
+        return item;
+      })
+    );
+  };
+
   return (
     <div className="todo">
-      <li className="todo-item">{task}</li>
-      <button className="complete-btn">
+      <li className={`todo-item ${item.completed ? "completed" : ""}`}>
+        {task}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHandler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -5,7 +5,13 @@ const Todo = ({ todo, setTodo }) => {
     <div className="todo-container">
       <ul className="todo-list">
         {todo.map((item) => (
-          <TaskItem task={item.task} key={item.id} />
+          <TaskItem
+            todo={todo}
+            setTodo={setTodo}
+            task={item.task}
+            key={item.id}
+            item={item}
+          />
         ))}
       </ul>
     </div>
